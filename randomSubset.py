b5c2ccf7e47365a62e1decd62bf0d1f9f7cc54c4
Gina Fenton <ginafenton123@gmail.com>
2026-03-21 15:48:54 +0000

    Merging the files successfully

--- randomSubset.py
+++ randomSubset.py
@@ -4,6 +4,13 @@
 import pandas as pd
 
 vents = pd.read_csv("Vent/vents_metadata.csv.bz2")
+emotions = pd.read_csv("Vent/emotions.csv")
 
 df = pd.DataFrame(vents)
-print(df.head())
+
+ventsMerg = vents.merge(emotions, left_on='emotion_id', right_on='id')
+
+ventswithEmotions = ventsMerg[['id', 'user_id', 'name', 'reactions']]
+ventswithEmotions = ventswithEmotions.rename(columns={'id': 'post_id', 'name': 'emotion'})
+
+print(ventswithEmotions.head())
